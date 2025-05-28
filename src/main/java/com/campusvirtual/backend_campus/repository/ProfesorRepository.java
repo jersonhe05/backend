cb36377c9e052bcd8f4e597250fbdd1b1afe74eb
Jbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-05-27 23:04:46 -0500

    creando repositorio para Profesor

---
+++ src/main/java/com/campusvirtual/backend_campus/repository/ProfesorRepository.java
@@ -0,0 +1,8 @@
+package com.campusvirtual.backend_campus.repository;
+
+import com.campusvirtual.backend_campus.entity.Profesor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfesorRepository extends JpaRepository<Profesor, Long> {
+
+}
