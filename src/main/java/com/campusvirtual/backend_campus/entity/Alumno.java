14863b8a7cfd9e4d622b442b57856969589ea1b5
Jbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-05-27 21:47:31 -0500

    creando clase Alumno con: - matriculas (a cursos) - entregas (de asignaciones) - cadenas (foros)

---
+++ src/main/java/com/campusvirtual/backend_campus/entity/Alumno.java
@@ -0,0 +1,28 @@
+package com.campusvirtual.backend_campus.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "alumnos")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Alumno extends  Usuario {
+
+    //cursos matriculados
+    @OneToMany(mappedBy = "alumno")
+    private List<Matricula> matriculas;
+
+    //Entregas realizadas
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Entrega> entregas;
+
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Cadena> cadenas;
+}
