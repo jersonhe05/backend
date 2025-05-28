08785b6f83c332fd74788c877614842f1e37f59a
Jbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-05-27 22:59:07 -0500

    fix super builder

--- src/main/java/com/campusvirtual/backend_campus/entity/Alumno.java
+++ src/main/java/com/campusvirtual/backend_campus/entity/Alumno.java
@@ -2,6 +2,7 @@ package com.campusvirtual.backend_campus.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 import java.util.List;
 
@@ -9,7 +10,7 @@ import java.util.List;
 @Table(name = "alumnos")
 @Getter
 @Setter
-@Builder
+@SuperBuilder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Alumno extends  Usuario {
