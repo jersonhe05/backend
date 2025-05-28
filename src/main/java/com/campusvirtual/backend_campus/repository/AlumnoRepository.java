be980e1ef0f439a4c4f645900102f67f5c73d15c
Jbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-05-27 23:00:11 -0500

    creando repositorio para alumnos

---
+++ src/main/java/com/campusvirtual/backend_campus/repository/AlumnoRepository.java
@@ -0,0 +1,8 @@
+package com.campusvirtual.backend_campus.repository;
+
+import com.campusvirtual.backend_campus.entity.Alumno;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlumnoRepository extends JpaRepository<Alumno, Long> {
+
+}
