ad4558a83171d544c3728c4d2dc10dd4d9cb0eea
Jbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-05-27 21:45:24 -0500

    fix: el curso tiene un listado de matriculas

--- src/main/java/com/campusvirtual/backend_campus/entity/Curso.java
+++ src/main/java/com/campusvirtual/backend_campus/entity/Curso.java
@@ -1,7 +1,11 @@
 package com.campusvirtual.backend_campus.entity;
 
+import com.campusvirtual.backend_campus.entity.util.Profesor;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "cursos")
@@ -25,9 +29,15 @@ public class Curso {
     @Column(length = 100)
     private String carrera;
 
-    @Column(length = 100)
-    private String profesor;
+    @ManyToOne
+    @JoinColumn(name = "id_profesor")
+    private Profesor profesor;
 
     @Column(length = 255)
     private String descripcion;
+
+    //El curso tiene muchas matriculas y cada matricula tiene curso y alumno
+    @OneToMany(mappedBy = "curso")
+    private List<Matricula> matriculas;
+
 }
